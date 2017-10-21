2d388b876ac6814bbeb0ba7cff264873f7aff1df
Akshay Pant <Akshay.Pant@iiitb.org>
2017-10-21 17:33:11 +0530

    Removed extra call.

--- js/vars.js
+++ js/vars.js
@@ -1,4 +1,4 @@
 var veg=0;
-var nodejsScript='https://78fce7e6.ngrok.io';//'https://bdd1-a432393.apaas.us2.oraclecloud.com';
-var redisDb='http://129.144.182.67:4000';
-var pythonSript='http://';
+var nodejsScript='https://1835eee1.ngrok.io';//'https://bdd1-a432393.apaas.us2.$
+var redisDb='http://35.154.42.243:4000';
+var pythonSript='http://35.154.42.243:4040';
